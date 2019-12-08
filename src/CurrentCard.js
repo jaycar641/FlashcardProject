1224ddbee48da2bc588c4e6bd9c10750c89fc220
Jacob Carson <jaycar641@yahoo.com>
2019-12-08 00:28:55 -0600

    Outline

---
+++ src/CurrentCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CurrentCard.css';
+
+class Card extends Component {
+
+//create api call to json collection, after click event for button created on top of sidebar
+
+
+
+
+
+
+
+}
