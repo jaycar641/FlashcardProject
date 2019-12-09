988ad08ed9047ae5f72003a8d9f26d5bd9c4a976
Jacob Carson <jaycar641@yahoo.com>
2019-12-09 15:44:26 -0600

    Classes set up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './CurrentCard.js';
+import Card from './CurrentCard.js';
+import Sidebar from './SideBar.js';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
+      <Card value = "Card" description = "Description" />
       </header>
 
-      
+     <Sidebar value = "C#"/>
+
+     <Sidebar value = "VSCode"/>
+
+
     </div>
   );
 }
+
+
 
 export default App;
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,2 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Sidebar.css';
+
+class Sidebar extends Component {
+state = {
+value: "C#",
+isActive: true
+
+}
+
+componentDidMount() {
+
+ this.setState({
+value: this.props.value,
+isActive: this.props.isActive
+ });   
+}
+
+//create api call to json collection, after click event for button created on top of sidebar
+
+render() {
+return (
+<div>
+<h2>Collection</h2>
+<h3>
+    {this.state.value}
+</h3>
+
+</div>
+
+
+);
+
+}
+
+
+}
+
+export default Sidebar;
