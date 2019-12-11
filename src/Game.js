ae5eb1f3a4e49f326a68f10fd058583a738968cb
Jacob Carson <jaycar641@yahoo.com>
2019-12-11 10:38:06 -0600

    finishing touches

--- src/Game.js
+++ src/Game.js
@@ -2,26 +2,31 @@ import React from 'react';
 import Card from './CurrentCard.js';
 import Sidebar from './SideBar.js';
 import './Game.css';
+import Axios from 'axios';
 
-class Game extends React.Component {
+ class Game extends React.Component {
     constructor (props) {
         super(props);
         this.state = {
     collection: null,
     isReact: null,
     counter: 0,
-    cards: Array(3).fill(null)
+    reactcards: [ ],
+    ccards: [ ],
     
         };
     }
     
     componentDidMount() {
         /*Call api and set results to the cards array*/
-     this.setState({
-    cards: [3, 6, 9, 12],
-    collection: this.state.isReact ? 'React' : 'C#'
+        Axios.get('http://localhost:3001/collections')
+        .then(res => { 
+            this.setState({
+                reactcards: res.data.slice(0,1),
+                ccards: res.data.slice(1,2)
+            })
+        });
 
-     });   
     }
 
     handleClick () {
@@ -44,18 +49,22 @@ class Game extends React.Component {
             });
         }
     }
-    renderCard() {
-        var description1 = this.state.cards[this.state.counter];
+    renderCard(counter) {
+
+
         if (this.state.isReact === true) {
 
             return (
-                <Card value = 'React' description = {description1} onClick ={() => this.handleClick()} />
+                
+                <Card value = "React" description = {this.state.reactcards.cards} onClick ={() => this.handleClick()} />
+                
+
                 );
 
         }
         else {
             return (
-                <Card value = 'C#' description = {description1} onClick ={() => this.handleClick()} />
+                <Card value = 'C#' description = "null" onClick ={() => this.handleClick()} />
                 );
         }
        
@@ -72,12 +81,14 @@ class Game extends React.Component {
     //create api call to json collection, after click event for button created on top of sidebar
     
     render() {
+       
+
+     
     return (
        <header>
+           
          <button>
-        {this.renderCard()}
-        <h1>{this.state.counter}</h1>
-        <h1>{this.state.isReact}</h1>
+        {this.renderCard(this.state.counter)}
         </button>
 
     <button>
@@ -86,7 +97,31 @@ class Game extends React.Component {
     <button>
         {this.renderSideBar("C#")}
     </button>
-    
+       {
+       this.state.reactcards.map(card => {  
+            return (
+              <h1>{card.title}
+               </h1> 
+              ); 
+
+              {this.state.reactcards.cards.map(definition => {
+                return (
+                    <h1>{definition.word}</h1>
+
+                );
+
+              })
+            }
+
+              
+         
+         })
+         
+         }
+            
+
+       <h1>{this.state.counter}</h1>
+
     </header>
 
 
@@ -96,8 +131,7 @@ class Game extends React.Component {
 }
     
     
+export default Game;    
     
-    
-    export default Game;
 
 
